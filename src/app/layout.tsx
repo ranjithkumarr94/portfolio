3e2291ab4ed44ac064826f5450e44f0b4113886d
Ranjith Kumar R <168267328+ranjithkumarr94@users.noreply.github.com>
2024-06-02 15:35:55 +0530

    refactor: format rule (#2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
 import type { Metadata } from "next";
 
-import "./globals.css";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Ranjith Kumar",
-	description: "This is my portfolio",
+  title: "Ranjith Kumar",
+  description: "This is my portfolio",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import NavBar from "./components/NavBar/NavBar";
 
 export default function Home() {
-	return (
-		<main>
-			<NavBar />
-		</main>
-	);
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
 }
