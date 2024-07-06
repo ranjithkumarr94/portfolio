250fe510551c6712fcdd58c3cb6f2012e3a49b98
ranjithkumarr94 <ranjithkumarr942001@gmail.com>
2024-07-06 17:45:01 +0530

    build: add mui and tailwind cleanup

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,14 +1,3 @@
-import NavLink from "../NavLink/NavLink";
-
 export default function NavBar() {
-  return (
-    <div className="flex items-center justify-around">
-      <div className="p-8 text-xl">Ranjith</div>
-      <div>
-        <ul>
-          <NavLink />
-        </ul>
-      </div>
-    </div>
-  );
+  return <>Hello i am ranjith</>;
 }
